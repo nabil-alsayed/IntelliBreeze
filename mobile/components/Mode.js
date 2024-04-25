f75106d01f5ffb3c0c1c991bf65916a899e79be5
nabilsa <nabilsa@student.chalmers.se>
2024-04-25 23:36:20 +0100

    #9 Update Styling for Mode.js and ModesDisplayWidget.js

--- mobile/components/Mode.js
+++ mobile/components/Mode.js
@@ -3,23 +3,34 @@ import { Text, View, StyleSheet } from "react-native";
 
 const MetricsDisplayWidget = (props) => {
   return (
-    <View style={styles.container}>
-      <Text> Mode </Text>
-    </View>
+      <View style={styles.mainContainer}>
+        <View style={styles.modeContainer}>
+          <Text> Mode </Text>
+        </View>
+        <Text style={styles.modeTitle}>Mode</Text>
+      </View>
   );
 };
 
 const styles = StyleSheet.create({
-  container: {
+  mainContainer:{
     flexDirection: "column",
+    justifyContent: "center",
+    alignItems: "center",
+    rowGap:5,
+  },
+  modeContainer: {
+    display: "flex",
+    width: 70,
+    height: 70,
     padding: 10,
-    justifyContent: "flex-start",
-    width: 120,
-    height: 150,
     justifyContent: "center",
     alignItems: "center",
     backgroundColor: "#fff",
     borderRadius: 20,
+  },
+  modeTitle:{
+    color:"#868585",
   },
 });
 
--- mobile/components/ModesDisplayWidget.js
+++ mobile/components/ModesDisplayWidget.js
@@ -1,26 +1,62 @@
 import React from "react";
-import { View, StyleSheet } from "react-native";
+import { Text, View, StyleSheet } from "react-native";
 import Mode from "./Mode";
+import { AntDesign } from '@expo/vector-icons';
 
-const MetricsDisplayWidget = (props) => {
+const ModesDisplayWidget = () => {
   return (
-    <View style={styles.container}>
-      <Mode />
-      <View style={styles.plus} />
+    <View style={styles.mainContainer}>
+      <Text style={styles.text}>General Modes</Text>
+        <View style={styles.modesContainer}>
+        <Mode />
+        <View style={styles.plusContainer}>
+          <View style={styles.plus} >
+            <AntDesign name={"plus"} size={24} color={"grey"} />
+          </View>
+          <Text style={styles.addTitle}>Add</Text>
+        </View>
+      </View>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
-  container: {
-    flexDirection: "row",
-    padding: 10,
+  mainContainer:{
+    flexDirection: "column",
     justifyContent: "flex-start",
     width: "100%",
-    justifyContent: "start",
-    backgroundColor: "#000",
+    rowGap: 15,
   },
-  plus: {},
+  text:{
+    color: "#000",
+    fontSize: 20,
+    fontWeight: 500,
+  },
+  addTitle:{
+    color: "#868585",
+  },
+  modesContainer: {
+    flexDirection: "row",
+    justifyContent: "flex-start",
+    width: "100%",
+    columnGap: 15,
+  },
+  plusContainer:{
+    flexDirection: "column",
+    justifyContent: "center",
+    alignItems: "center",
+    rowGap:5,
+  },
+  plus: {
+    display: "flex",
+    width: 70,
+    height: 70,
+    padding: 10,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#fff",
+    borderRadius: 20,
+  },
 });
 
-export default MetricsDisplayWidget;
+export default ModesDisplayWidget;
