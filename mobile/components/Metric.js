a854022f263ed46459e9951d5ada9a4f542da717
nabilsa <nabilsa@student.chalmers.se>
2024-04-27 20:27:27 +0100

    #9 Import useEffect and useState to Implement the Conversion of Unit

--- mobile/components/Metric.js
+++ mobile/components/Metric.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import {StyleSheet, Text, View} from "react-native";
 import {FontAwesome6} from '@expo/vector-icons';
 
