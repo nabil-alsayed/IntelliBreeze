339c9b1aa32a5c0848f84c55655df08d05a01100
Manas Ahuja <manasah@student.chalmers.se>
2024-04-27 19:08:06 +0200

    #25 Finish sliders with  MEDIUM to HIGH slider
    Both the sliders were aligned and labelled with their readings displayed. A react-native library from fortawesome was imported to have a fan icon next to the label enhancing the UI for better communication with the user

--- mobile/screens/TemperatureThresholdSettings.js
+++ mobile/screens/TemperatureThresholdSettings.js
@@ -1,6 +1,11 @@
 import React, { useEffect, useState } from "react";
 import { View, Text, StyleSheet, Image } from "react-native";
 import Slider from "@react-native-community/slider";
+import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome';
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faFan } from "@fortawesome/free-solid-svg-icons";
+
+library.add(faFan);
 
 export default function TemperatureThresholdSettings() {
     const [range, setRange] = useState(0);
@@ -8,7 +13,7 @@ export default function TemperatureThresholdSettings() {
 
     const convertTemperature = (temp) => { //This function calculates the temperature if the preferredUnit is changed
         if (preferredUnit === 'F') {
-            return Math.floor(Math.round((temp * 9/5) + 32)) + '°F';
+            return Math.floor(Math.round((temp * 9 / 5) + 32)) + '°F';
         } else if (preferredUnit === 'K') {
             return Math.floor((temp + 273.15).toFixed(2)) + 'K';
         }
@@ -16,22 +21,27 @@ export default function TemperatureThresholdSettings() {
     }
 
     useEffect(() => { //This function allows modification of the preferredUnit test variable
-        setPreferredUnit('K');
+        setPreferredUnit('F');
     }, []);
 
     return (
         <View style={styles.container}>
+            {/*LOW to MEDIUM Slider begins here*/}
             <View style={styles.header}>
                 <Text style={styles.headerText}>Temperature Threshold Settings</Text>
             </View>
             <View style={styles.line}></View>
-            <View style={styles.adjustmentContainer}>
+            <View style={styles.adjustmentContainerLM}>
                 <Image
                     source={require('../assets/OtherIcons/coldlogo.png')}
                     style={styles.coldLogo}
                 />
                 <View style={styles.sliderWrapper}>
-                    <Text style={styles.thresholdLabel}>LOW to MEDIUM Threshold: {convertTemperature(range)}</Text>
+                    <View style={{ flexDirection: 'row', alignItems: 'center', justifyContent: 'center' }}>
+                        <Text style={styles.thresholdLabel}>LOW to MEDIUM </Text>
+                        <FontAwesomeIcon icon={faFan} style={[{ color: 'black' }, { marginBottom: 15 }, { marginLeft: 5 }]} size={30} />
+                    </View>
+                    <Text style={{ fontSize: 18, fontWeight: 'normal', color: 'black', marginBottom: 10, marginRight: 10 }}>Threshold: {convertTemperature(range)}</Text>
                     <Slider
                         style={{ width: 300, height: 50 }}
                         onValueChange={(value) => setRange(value)}
@@ -44,8 +54,33 @@ export default function TemperatureThresholdSettings() {
                     style={styles.hotLogo}
                 />
             </View>
+
             <View style={styles.line}></View>
 
+            {/*MEDIUM to HIGH Slider begins here*/}
+            <View style={styles.adjustmentContainerMH}>
+                <Image
+                    source={require('../assets/OtherIcons/coldlogo.png')}
+                    style={styles.coldLogo}
+                />
+                <View style={styles.sliderWrapper}>
+                    <View style={{ flexDirection: 'row', alignItems: 'center', justifyContent: 'center' }}>
+                        <Text style={styles.thresholdLabel}>MEDIUM to HIGH </Text>
+                        <FontAwesomeIcon icon={faFan} style={[{ color: 'black' }, { marginBottom: 15 }, { marginLeft: 5 }]} size={30} />
+                    </View>
+                    <Text style={{ fontSize: 18, fontWeight: 'normal', color: 'black', marginBottom: 10, marginRight: 10 }}>Threshold: {convertTemperature(range)}</Text>
+                    <Slider
+                        style={{ width: 300, height: 50 }}
+                        onValueChange={(value) => setRange(value)}
+                        minimumValue={-50}
+                        maximumValue={100}
+                    />
+                </View>
+                <Image
+                    source={require('../assets/OtherIcons/hotlogo.png')}
+                    style={styles.hotLogo}
+                />
+            </View>
         </View>
     );
 }
@@ -73,10 +108,8 @@ const styles = StyleSheet.create({
         borderBottomWidth: 1,
         borderBottomColor: "#ccc",
         width: "100%",
-        marginTop: 5,
-    },
-    logoContainer: {
-        marginLeft: 10,
+        marginTop: 20,
+        marginBottom: 1
     },
     hotLogo: {
         width: 40,
@@ -86,18 +119,25 @@ const styles = StyleSheet.create({
         width: 50,
         height: 50,
     },
-    adjustmentContainer: {
+    adjustmentContainerLM: {   /*adjustment container for the low to medium slider*/
         flexDirection: "row",
         alignItems: "center",
         justifyContent: "space-between",
-        marginTop: 180,
+        marginTop: 220,
+    },
+    adjustmentContainerMH: { /*adjustment container for the medium to high slider*/
+        flexDirection: "row",
+        alignItems: "center",
+        justifyContent: "space-between",
+        marginTop: 70,
     },
     thresholdLabel: {
         fontSize: 18,
         fontWeight: "bold",
         marginBottom: 10,
+
     },
     sliderWrapper: {
         alignItems: "center",
-    }
+    },
 });
