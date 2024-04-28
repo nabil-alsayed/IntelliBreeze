ba07cdf93bb2e6ac9066fb3f97c7d8ae369197b8
nabilsa <nabilsa@student.chalmers.se>
2024-04-28 01:49:11 +0100

    #9 Implement The useState Needed for Modal Visibility and fanSpeed

--- mobile/components/AddModeForm.js
+++ mobile/components/AddModeForm.js
@@ -2,3 +2,12 @@ import React, { useState } from 'react';
 import { Picker } from "react-native-web";
 import { Modal, View, TextInput, Text, Button, StyleSheet } from 'react-native';
 import MultiSlider from '@ptomasroos/react-native-multi-slider';
+
+const AddModeForm = () => {
+
+    const [modalVisible, setModalVisible] = useState(false);
+    const [fanSpeed, setFanSpeed] = useState(1);
+
+}
+
+export default AddModeForm;
