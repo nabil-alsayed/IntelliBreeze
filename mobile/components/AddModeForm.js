6825413d6cd7f2f619129bb54f8e4f1383ab0f37
nabilsa <nabilsa@student.chalmers.se>
2024-04-28 01:45:31 +0100

    #9 Import Necessary Elements/Libraries to Implement the Form

---
+++ mobile/components/AddModeForm.js
@@ -0,0 +1,3 @@
+import React, { useState } from 'react';
+import { Modal, View, TextInput, Picker, Text, Button, StyleSheet } from 'react-native';
+import MultiSlider from '@ptomasroos/react-native-multi-slider';
