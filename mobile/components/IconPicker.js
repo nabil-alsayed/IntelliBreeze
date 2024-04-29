558c9bd176af0bd8a111e1180c94cafb79fd0ea4
nabilsa <nabilsa@student.chalmers.se>
2024-04-29 13:18:07 +0100

    #9 Add a List of Icons to Use in the Picker

--- mobile/components/IconPicker.js
+++ mobile/components/IconPicker.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import { View } from 'react-native';
 const IconPicker = () => {
+
+    // The list of Icons to use in the picker
+    const icons = ['bolt','filter','bed', 'sun-o', 'money','book', 'moon-o', 'heart','magic','paw','smile-o','refresh','leaf','tint','gamepad','cog','home','lock'];
+
     return (
         <View style = {styles.container}>
 
