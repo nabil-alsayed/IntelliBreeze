95d31433cb426f6b361b5a56463925b000ef2ae8
nabilsa <nabilsa@student.chalmers.se>
2024-04-29 13:16:35 +0100

    #9 Create Skeleton of Custom IconPicker For Modes Creation

---
+++ mobile/components/IconPicker.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { View } from 'react-native';
+const IconPicker = () => {
+    return (
+        <View style = {styles.container}>
+
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    container:{
+        flex:1,
+    }
+})
+
+export default IconPicker;
