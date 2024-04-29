504e300cf98c18b7942e9db0054d0e5bc4815343
nabilsa <nabilsa@student.chalmers.se>
2024-04-29 13:57:50 +0100

    #9 Add Necessary TextInput and Label with Styling to the Form

--- mobile/components/AddModeForm.js
+++ mobile/components/AddModeForm.js
@@ -1,18 +1,41 @@
 import React, { useState } from 'react';
-import {View, StyleSheet} from 'react-native';
+import {Text, View, StyleSheet, TextInput} from 'react-native';
 
 const AddModeForm = () => {
 
     const [modeName, setModeName ] = useState('');
 
     return (
-        <View style={styles.container}>
+        <View style={styles.form}>
+            <Text style={{fontWeight:"600",fontSize:15,}}>Mode Name</Text>
+            <TextInput
+                style={styles.input}
+                onChangeText={setModeName}
+                value={modeName}
+                placeholder="Mode Name"
+            />
         </View>
     )
 }
 
 const styles = StyleSheet.create({
-    container:{},
+    form: {
+        flexDirection: "column",
+        width:"100%",
+        alignItems: "flex-start",
+        rowGap:10,
+        backgroundColor:"white",
+        padding:20,
+        borderRadius:20,
+    },
+    input: {
+        height: 40,
+        padding:5,
+        borderColor: "lightgrey",
+        borderWidth: 2,
+        borderRadius:10,
+        width:"100%",
+    },
 })
 
 export default AddModeForm;
