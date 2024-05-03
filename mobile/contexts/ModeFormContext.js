70ebab58845fd88f9a6f145e57bb1849f7d3a921
nabilsa <nabilsa@student.chalmers.se>
2024-05-03 06:03:26 +0200

    #14 Advance ModeFormContext.js to hold Responsibility for Form Global States

--- mobile/contexts/ModeFormContext.js
+++ mobile/contexts/ModeFormContext.js
@@ -1,3 +1,33 @@
 import { createContext } from 'react';
 
 export const ModeFormContext = createContext();
+
+export const ModeFormProvider = ({ children }) => {
+    const [modalVisible, setModalVisible] = useState(false);
+    const [modeEditModalVisible, setModeEditModalVisible] = useState(false);
+    const [fanSpeed, setFanSpeed] = useState(0);
+    const [modes, setModes] = useState([]);
+    const [formValid, setFormValid] = useState(false);
+    const [modeConfirmationModalVisible, setModeConfirmationModalVisible] = useState(false);
+
+    const value = {
+        modalVisible,
+        setModalVisible,
+        modeEditModalVisible,
+        setModeEditModalVisible,
+        modeConfirmationModalVisible,
+        setModeConfirmationModalVisible,
+        formValid,
+        setFormValid,
+        fanSpeed,
+        setFanSpeed,
+        modes,
+        setModes
+    };
+
+    return (
+        <ModeFormContext.Provider value={value}>
+            {children}
+        </ModeFormContext.Provider>
+    );
+};
