b772bf5a2f76e327368564c2a9707e3b460eb7be
nabilsa <nabilsa@student.chalmers.se>
2024-05-03 05:40:41 +0200

    #14 Import useEffect for the onSnapshot Realtime/Quick Firebase Changes

--- mobile/components/AddModeForm.js
+++ mobile/components/AddModeForm.js
@@ -1,4 +1,4 @@
-import React, {useContext, useState} from 'react';
+import React, {useContext, useEffect, useState} from 'react';
 import {Text, View, StyleSheet, TextInput, Dimensions, Pressable} from 'react-native';
 import MultiSlider from '@ptomasroos/react-native-multi-slider';
 import IconPicker from "./IconPicker";
