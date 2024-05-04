51a7512e3569b3d68264d412d4b5e2cd009eca14
nabilsa <nabilsa@student.chalmers.se>
2024-05-05 01:00:42 +0200

    #16 Use the Custom useTemperatureSubscription.js hook in MetricsDisplayWidget.js

--- mobile/components/MetricsDisplayWidget.js
+++ mobile/components/MetricsDisplayWidget.js
@@ -1,19 +1,26 @@
-import React from "react";
+import React, {useState} from "react";
 import { View, StyleSheet } from "react-native";
 import Metric from "./Metric";
+import {useTemperatureSubscription} from "../hooks/useTemperatureSubscription";
 
-const MetricsDisplayWidget = (props) => {
-  return (
-    <View style={styles.container}>
-      <Metric
-        iconName="temperature-half"
-        metricName="Temperature"
-        metricValue={props.value}
-        metricUnit="°C"
-      />
-      <Metric iconName= "droplet" metricName="Humidity" metricValue="20" metricUnit="%" />
-    </View>
-  );
+const MetricsDisplayWidget = () => {
+    const [temperature, setTemperature] = useState(0);
+
+    useTemperatureSubscription((newTemperature) => {
+        setTemperature(newTemperature);
+    });
+
+    return (
+        <View style={styles.container}>
+            <Metric
+                iconName="temperature-half"
+                metricName="Temperature"
+                metricValue={temperature}
+                metricUnit="°C"
+            />
+            <Metric iconName="droplet" metricName="Humidity" metricValue="20" metricUnit="%" />
+        </View>
+    );
 };
 
 const styles = StyleSheet.create({
