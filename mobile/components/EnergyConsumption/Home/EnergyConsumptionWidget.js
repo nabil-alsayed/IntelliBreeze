2719b367d986f7f71860597c624a7c8e626cf851
nabilsa <nabilsa@student.chalmers.se>
2024-05-04 03:59:06 +0200

    #16 Import Necessary Elements & Components to Implement Widget/ Design

---
+++ mobile/components/EnergyConsumption/Home/EnergyConsumptionWidget.js
@@ -0,0 +1,3 @@
+import React, { useState, useContext } from 'react'
+import {Text, StyleSheet, View, TouchableOpacity} from "react-native";
+import Icon from "react-native-vector-icons/FontAwesome";
