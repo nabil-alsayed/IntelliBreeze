1b809bf5c0b4b4a5fbabcc71fd56be93850d951b
nabilsa <nabilsa@student.chalmers.se>
2024-05-04 04:04:41 +0200

    #16 Add Styles for Main Container and Icon Container

--- mobile/components/EnergyConsumption/Home/EnergyConsumptionWidget.js
+++ mobile/components/EnergyConsumption/Home/EnergyConsumptionWidget.js
@@ -24,4 +24,24 @@ const EnergyConsumptionWidget = ( { value = 20, unit = "kWh" }  ) => {
     )
 }
 
+const styles = StyleSheet.create({
+    container:{
+        justifyContent:"flex-start",
+        alignItems:"center",
+        padding:15,
+        height:100,
+        borderRadius:20,
+        backgroundColor:"#fff",
+        columnGap:10,
+        flexDirection:"row"
+    },
+    iconContainer:{
+        width:60,
+        height:60,
+        justifyContent:"center",
+        alignItems:"center",
+        backgroundColor: "#f3f3f3",
+        borderRadius:50
+    }
+})
 export default EnergyConsumptionWidget;
