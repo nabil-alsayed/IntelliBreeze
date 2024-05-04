eeaa2f7ea6b2f42e41e91e7cb73500596987a16f
nabilsa <nabilsa@student.chalmers.se>
2024-05-05 00:58:54 +0200

    #16 Implement Temporary Structure for EnergyConsumptionStats.js Screen

---
+++ mobile/screens/EnergyConsumptionStats.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+const EnergyConsumptionStats = () => {
+
+    return (
+        <View>
+            <Text>{this.name}</Text>
+        </View>
+    )
+}
+
+export default EnergyConsumptionStats;
