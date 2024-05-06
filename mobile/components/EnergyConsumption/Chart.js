df85e0fee892bf33ec0eeb50befa29a137e1a32b
nabilsa <nabilsa@student.chalmers.se>
2024-05-07 01:22:32 +0200

    #16 Import Necessary Libraries to Implement the Chart

---
+++ mobile/components/EnergyConsumption/Chart.js
@@ -0,0 +1,5 @@
+import React, { useState } from 'react';
+import {View, StyleSheet, Dimensions, Text, Pressable} from 'react-native';
+import { EnergyData } from '../../data/Data';
+
+const screenWidth = Dimensions.get('window').width;
