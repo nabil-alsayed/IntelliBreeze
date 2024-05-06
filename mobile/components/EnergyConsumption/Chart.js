8c86be70eb1052ee405f202661d88d39855dfff9
nabilsa <nabilsa@student.chalmers.se>
2024-05-07 01:44:07 +0200

    #16 Implement Line Chart using rn chart-kit Mapping it to Different Timeframes

--- mobile/components/EnergyConsumption/Chart.js
+++ mobile/components/EnergyConsumption/Chart.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import {View, StyleSheet, Dimensions, Text, Pressable} from 'react-native';
+import { LineChart } from 'react-native-chart-kit';
 import { EnergyData } from '../../data/Data';
 
 const screenWidth = Dimensions.get('window').width;
@@ -13,7 +14,45 @@ const EnergyConsumptionStats = () => {
     };
 
     return (
-        <View>
+        <View style={styles.container}>
+            <View>
+                <LineChart
+                    data={chartData}
+                    width={screenWidth - 32}
+                    height={220}
+                    chartConfig={{
+                        backgroundColor: '#1682d0',
+                        backgroundGradientFrom: '#2695e3',
+                        backgroundGradientTo: '#185191',
+                        color: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
+                        labelColor: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
+                        style: {
+                            borderRadius: 16,
+                        },
+                        propsForDots: {
+                            r: '3',
+                            strokeWidth: '2',
+                            stroke: '#ffffff',
+                        }
+                    }}
+                    bezier
+                    style={{
+                        marginVertical: 8,
+                        borderRadius: 16,
+                    }}
+                />
+            </View>
+            <View >
+                {Object.keys(EnergyData).map((key) => (
+                    <Pressable
+                        key={key}
+                        title={key.charAt(0).toUpperCase() + key.slice(1)}
+                        onPress={() => setTimeframe(key)}
+                    >
+                                <Text>{key}</Text>
+                    </Pressable>
+                ))}
+            </View>
         </View>
     );
 };
