fe84fc94f472bea0ffb55d4ead0812a73bec45f7
nabilsa <nabilsa@student.chalmers.se>
2024-05-07 01:25:45 +0200

    #16 Initialise chartData & timeFrame State

--- mobile/components/EnergyConsumption/Chart.js
+++ mobile/components/EnergyConsumption/Chart.js
@@ -3,3 +3,19 @@ import {View, StyleSheet, Dimensions, Text, Pressable} from 'react-native';
 import { EnergyData } from '../../data/Data';
 
 const screenWidth = Dimensions.get('window').width;
+
+const EnergyConsumptionStats = () => {
+    const [timeframe, setTimeframe] = useState('day');
+
+    const chartData = {
+        labels: EnergyData[timeframe].labels,
+        datasets: EnergyData[timeframe].datasets,
+    };
+
+    return (
+        <View>
+        </View>
+    );
+};
+
+export default EnergyConsumptionStats;
