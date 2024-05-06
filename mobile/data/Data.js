4b1991768aae33c05e9eff4c8acf216547d8b2df
nabilsa <nabilsa@student.chalmers.se>
2024-05-06 21:18:12 +0200

    #16 Design Temporary Structure of Data to Mimic Energy Consumed by the Fan

---
+++ mobile/data/Data.js
@@ -0,0 +1,18 @@
+export const EnergyData = {
+            minute: {
+                labels: ["00:00", "00:15", "00:30", "00:45"],
+                datasets: [{ data: [20, 45, 28, 80] }]
+            },
+            hour: {
+                labels: ["1 AM", "2 AM", "3 AM", "4 AM"],
+                datasets: [{ data: [30, 200, 170, 250] }]
+            },
+            day: {
+                labels: ["Mon", "Tue", "Wed", "Thu", "Fri","Sat"],
+                datasets: [{ data: [300, 500, 700, 600, 700, 1000] }]
+            },
+            week: {
+                labels: ["Week 1", "Week 2", "Week 3", "Week 4"],
+                datasets: [{ data: [800, 1230, 750, 950] }]
+            }
+}
