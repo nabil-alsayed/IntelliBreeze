c7a73336910e8190327deb5c44cc875318bc36e5
nabilsa <nabilsa@student.chalmers.se>
2024-05-07 03:12:08 +0200

    #16 Make Energy Widget Icon Pressable to toggle BlurryModal/Chart visibility

--- mobile/components/EnergyConsumption/EnergyConsumptionWidget.js
+++ mobile/components/EnergyConsumption/EnergyConsumptionWidget.js
@@ -1,5 +1,5 @@
 import React, { useState, useContext } from 'react'
-import {Text, StyleSheet, View, TouchableOpacity} from "react-native";
+import {Text, StyleSheet, View, TouchableOpacity,Pressable} from "react-native";
 import Icon from "react-native-vector-icons/FontAwesome";
 
 const EnergyConsumptionWidget = ( { value = 20, unit = "kWh" }  ) => {
@@ -16,9 +16,9 @@ const EnergyConsumptionWidget = ( { value = 20, unit = "kWh" }  ) => {
 
     return (
         <View style={styles.container}>
-            <View style={styles.iconContainer}>
-                <Icon  name={"bolt"} size={30}></Icon>
-            </View>
+            <Pressable style={styles.iconContainer} onPress={handleToggleModal}>
+                <Icon  name={"bolt"} size={30}/>
+            </Pressable>
             <View>
                 <View style={styles.value}>
                     <Text numberOfLines={1} style={{fontSize:25,fontWeight:"bold"}}> {value + " " + unit}</Text>
