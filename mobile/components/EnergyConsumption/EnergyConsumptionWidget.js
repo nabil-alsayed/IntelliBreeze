8e77a22983415606ac2220896a2e57c262c7ba7a
nabilsa <nabilsa@student.chalmers.se>
2024-05-08 20:46:11 +0200

    #16 Fetch Energy form Firebase, Update at Changes and Pass it To chart to Display for Day, Week & Month

--- mobile/components/EnergyConsumption/EnergyConsumptionWidget.js
+++ mobile/components/EnergyConsumption/EnergyConsumptionWidget.js
@@ -1,18 +1,38 @@
-import React, { useState, useContext } from 'react'
+import React, { useState, useEffect } from 'react'
 import {Text, StyleSheet, View, TouchableOpacity,Pressable} from "react-native";
 import Icon from "react-native-vector-icons/FontAwesome";
 import Chart from "./Chart";
 import BlurryModal from "../BlurryModal";
+import moment from 'moment';
+import { useTopicSubscription } from '../../hooks/useTopicSubscription';
+import { db } from "../../firebaseConfig";
+import { doc, setDoc, updateDoc, getDoc } from "firebase/firestore";
+import { FAN_SPEED } from '../../constants/MqttTopics'
+
+const initialEnergyData = () => {
+    let data = Array(31).fill(0);
+    return {
+        labels: [
+            "Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun",
+            ...Array.from({ length: 6 }, (_, i) => `Week ${moment().subtract(5 - i, 'weeks').isoWeek()}`),
+            "Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sept", "Oct", "Nov", "Dec"
+        ],
+        datasets: [{ data }]
+    };
+};
+
 
 const EnergyConsumptionWidget = ( { value = 20, unit = "kWh" }  ) => {
-    const [timeFrame, setTimeFrame] = useState("minute");
+    const [timeFrame, setTimeFrame] = useState("day");
     const [counter, setCounter] = useState(0);
     const [modalVisible, setModalVisible] = useState({visible: false, timeframe: timeFrame})
-    const frames = ["minute","hour","day", "week"];
+    const frames = ["day","week","month"];
+    const [energyData, setEnergyData] = useState(initialEnergyData());
+    const [currentDay, setCurrentDay] = useState(moment().format('ddd'));
 
     const handleToggleFrame = () => {
         const newCounter = (counter + 1) % frames.length; // Calculate next index
-        setCounter(newCounter); // Update the counter state
+        setCounter(newCounter);
         setTimeFrame(frames[newCounter]); // Update the time frame based on new counter
     };
 
@@ -22,6 +42,117 @@ const EnergyConsumptionWidget = ( { value = 20, unit = "kWh" }  ) => {
             timeframe: timeFrame
         })
     }
+
+    useEffect(() => {
+        const fetchData = async () => {
+            const paths = [
+                `Year/${moment().format('YYYY')}`,
+                `Month/${moment().format('MMMM')}`,
+                `Week/Week ${moment().isoWeek()}`,
+                `Day/${moment().format('ddd')}`
+            ];
+
+            const dataPromises = paths.map(async (path) => {
+                const docRef = doc(db, path);
+                const docSnap = await getDoc(docRef);
+                if (docSnap.exists()) {
+                    return { path, energy: docSnap.data().energy };
+                }
+                return { path, energy: 0 }; // default to 0 if not found
+            });
+
+            const results = await Promise.all(dataPromises);
+            const newData = { ...energyData };
+            results.forEach((result) => {
+                const { path, energy } = result;
+                const label = path.split('/').pop();
+                const index = newData.labels.findIndex(l => l.includes(label));
+                if (index !== -1) {
+                    newData.datasets[0].data[index] = energy;
+                }
+            });
+            setEnergyData(newData);
+        };
+
+        fetchData().then(r => {});
+    }, []);
+
+    useTopicSubscription(async (newEnergyData) => {
+        if (newEnergyData) {
+            const today = moment();
+            const yearPath = `Year/${today.format('YYYY')}`
+            const monthPath = `Month/${today.format('MMMM')}`
+            const weekPath = `Week/Week ${today.isoWeek()}`
+            const dayPath = `Day/${today.format('ddd')}`;
+            const energyIncrement = parseFloat(newEnergyData) * 0.5;
+
+            // Update Firestore for day, week, and month
+            const updateFirestore = async (path, increment) => {
+                const docRef = doc(db, path);
+                try {
+                    const docSnap = await getDoc(docRef);
+
+                    if (docSnap.exists()) {
+                        // Document exists, fetch current energy and add the new increment
+                        const currentEnergy = docSnap.data().energy || 0; // Handle case where energy might not be set
+                        await updateDoc(docRef, {
+                            energy: currentEnergy + increment
+                        });
+                    } else {
+                        // Document does not exist, create it with th initial value
+                        await setDoc(docRef, { energy: increment });
+                    }
+                } catch (error) {
+                    console.error("Error accessing or updating the document:", error);
+                }
+            };
+
+            // Update all timeframes with energy increment value
+            await Promise.all([
+                updateFirestore(dayPath, energyIncrement),
+                updateFirestore(weekPath, energyIncrement),
+                updateFirestore(monthPath, energyIncrement),
+                updateFirestore(yearPath, energyIncrement),
+            ]);
+
+            // Update local state
+            setEnergyData(prevData => {
+                const newData = {...prevData};
+                const dayIndex = prevData.labels.indexOf(moment().format('ddd'));
+                const weekIndex = prevData.labels.indexOf(`Week ${moment().isoWeek()}`);
+                const monthIndex = prevData.labels.indexOf(moment().format('MMMM'));
+
+                if (dayIndex !== -1) newData.datasets[0].data[dayIndex] += energyIncrement;
+                if (weekIndex !== -1) newData.datasets[0].data[weekIndex] += energyIncrement;
+                if (monthIndex !== -1) newData.datasets[0].data[monthIndex] += energyIncrement;
+
+                return newData;
+            });
+        }
+    }, FAN_SPEED.FAN_SPEED_TOPIC, FAN_SPEED.TOPIC_NAME);
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+            const today = moment().format('ddd');
+            if (today !== currentDay) {
+                setCurrentDay(today);
+                rollOverData(today);
+            }
+        }, 86400000); // 24 hours
+        return () => clearInterval(interval);
+    }, [currentDay]);
+
+    const rollOverData = today => {
+        setEnergyData(prevData => {
+            const newData = {...prevData};
+            if (moment(today).day() === 1) {
+                newData.labels.splice(7, 6, ...Array.from({length: 6}, (_, i) => `Week ${moment().subtract(i, 'weeks').isoWeek()}`));
+                newData.datasets[0].data.fill(0, 7, 13);
+            }
+            newData.datasets[0].data.fill(0, 0, 7);
+            return newData;
+        });
+    };
 
     return (
         <View style={styles.container}>
@@ -43,7 +174,7 @@ const EnergyConsumptionWidget = ( { value = 20, unit = "kWh" }  ) => {
                 visible={modalVisible.visible}
                 onClose={() => setModalVisible({visible: false, timeframe: timeFrame})}
             >
-                <Chart TimeFrame={modalVisible.timeframe}/>
+                <Chart selectedTimeFrame={modalVisible.timeframe} energyData={energyData}/>
             </BlurryModal>
         </View>
     )
