a566f1ca95c4ce1b0edeacc3cd759fdf7d459ac6
nabilsa <nabilsa@student.chalmers.se>
2024-05-10 20:06:22 +0200

    #34 Add MIN and MAX SPEED LIMITS constants for Modes Configuration to Constants file

--- mobile/constants/LogicConstants.js
+++ mobile/constants/LogicConstants.js
@@ -1,9 +1,11 @@
-export const FAN_SPEED =  {
+export const AUTO_FAN_SPEED =  {
     FAN_SPEED_TOPIC: "/intellibreeze/sensor/automatic/fanspeed",
     TOPIC_NAME: "AUTOMATIC FAN SPEED"
 }
 
 export const CUSTOM_FAN_SPEED = {
     CUSTOM_FAN_SPEED_TOPIC: "/intellibreeze/sensor/custom/fanspeed",
-    TOPIC_NAME: "CUSTOM FAN SPEED"
+    TOPIC_NAME: "CUSTOM FAN SPEED",
+    MIN_SPEED_LIMIT: 10,
+    MAX_SPEED_LIMIT: 256
 }
