47752102253f16ce81f2f5972f3ebc913512c593
nabilsa <nabilsa@student.chalmers.se>
2024-05-10 04:13:23 +0200

    #34 Add a Topic Constant for Publishing The Custom Mode's Fan Speed

--- mobile/constants/MqttTopics.js
+++ mobile/constants/MqttTopics.js
@@ -2,3 +2,8 @@ export const FAN_SPEED =  {
     FAN_SPEED_TOPIC: "/intellibreeze/sensor/automatic/fanspeed",
     TOPIC_NAME: "ENERGY DATA"
 }
+
+export const CUSTOM_FAN_SPEED = {
+    CUSTOM_FAN_SPEED_TOPIC: "/intellibreeze/sensor/custom/fanspeed",
+    TOPIC_NAME: "CUSTOM FAN SPEED"
+}
