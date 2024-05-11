4f8456d264a8a58f06fc28ad232268c42ee67abb
nabilsa <nabilsa@student.chalmers.se>
2024-05-11 11:16:01 +0200

    #34 Define customFanSpeedValue as Extern to Use it in Other Classes

--- WioTerminal/FanSpeedAdjustment.h
+++ WioTerminal/FanSpeedAdjustment.h
@@ -5,6 +5,7 @@
 
 extern String mediumThresholdValue;  //value set by user in the application where the fan switches speed to MEDIUM
 extern String highThresholdValue;    //value set by user in the application where the fan switches speed to HIGH
+extern String customFanSpeedValue;    // value set by user in the application for fan speed for a specific custom mode
 extern int dutyCycle;                //used to control the pulse width modulation of the fan
 extern float tempValue;              //used to compare user thresholds to control fan speed
 
