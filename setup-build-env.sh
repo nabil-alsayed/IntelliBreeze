061135eca1ab9162ea6ce58e38d373dfd6917ce0
Raghav Khurana <raghavk.chalmers.student.se>
2024-05-13 00:53:49 +0200

    #48 Revert Shell file and .gitlab-ci.yml file to previous versions

--- setup-build-env.sh
+++ setup-build-env.sh
@@ -1,5 +1,3 @@
-#!/bin/bash
-
 apt-get update
 cd ~
 
@@ -20,13 +18,10 @@ arduino-cli lib install "PubSubClient@2.8"
 arduino-cli lib install "TFT_eSPI@2.5.43"
 arduino-cli lib install "DHT sensor library@1.4.6"
 
-#Install Node.js
 
-apt-get install curl -y
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
-# Download and install nvm
-export PATH="$NVM_DIR/bin:$PATH" # Add nvm executable to PATH
+cd -
+
+
+
 
 
