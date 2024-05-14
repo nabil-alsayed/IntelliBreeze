fc498081aa70f137bcde6664a20c22a9faa4dd22
nabilsa <nabilsa@student.chalmers.se>
2024-05-15 00:02:49 +0200

    #20 Move Context From HomeScreen.js to a Higher Hierarchy in App.js Instead

--- mobile/App.js
+++ mobile/App.js
@@ -1,9 +1,12 @@
 import Stacks from './navigations/Stacks'
+import {ModeFormProvider} from './contexts/ModeFormContext'
 
 export default function App() {
 
     return (
-        <Stacks/>
+        <ModeFormProvider>
+            <Stacks/>
+        </ModeFormProvider>
     );
 
 }
