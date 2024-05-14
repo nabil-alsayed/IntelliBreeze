75656a26f635c5bab7b472223adbd72a38a95328
nabilsa <nabilsa@student.chalmers.se>
2024-05-14 20:56:26 +0200

    #20 Add Selected Mode Fan Speed & Fan Status to Global State/Context

--- mobile/contexts/ModeFormContext.js
+++ mobile/contexts/ModeFormContext.js
@@ -6,10 +6,12 @@ export const ModeFormProvider = ({ children }) => {
     const [modalVisible, setModalVisible] = useState(false);
     const [modeEditModalVisible, setModeEditModalVisible] = useState(false);
     const [fanSpeed, setFanSpeed] = useState(0);
+    const [fanIsOn, setFanIsOn] = useState(true)
     const [modes, setModes] = useState([]);
     const [formValid, setFormValid] = useState(false);
     const [modeConfirmationModalVisible, setModeConfirmationModalVisible] = useState(false);
     const [selectedModeId, setSelectedModeId] = useState("auto")
+    const [selectedModeFanSpeed, setSelectedModeFanSpeed] = useState(0)
     const value = {
         modalVisible,
         setModalVisible,
@@ -21,10 +23,14 @@ export const ModeFormProvider = ({ children }) => {
         setFormValid,
         fanSpeed,
         setFanSpeed,
+        fanIsOn,
+        setFanIsOn,
         modes,
         setModes,
         selectedModeId,
         setSelectedModeId,
+        selectedModeFanSpeed,
+        setSelectedModeFanSpeed
     };
 
     return (
