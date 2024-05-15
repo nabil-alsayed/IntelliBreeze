2fc3f76b6e2c7820f1342ba44f59662d2a69b2d4
nabilsa <nabilsa@student.chalmers.se>
2024-05-15 05:01:49 +0200

    #40 Remove Unused Import & Import Method from temperatureConverterUtils to display

--- mobile/components/TemperatureThresholds/TemperatureSlider.js
+++ mobile/components/TemperatureThresholds/TemperatureSlider.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { View, Text, StyleSheet, Image, Animated } from 'react-native';
 import Slider from '@react-native-community/slider';
-import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome';
+import { convertTemperature } from '../../utils/temperatureConverterUtils'
 
 const TemperatureSlider = ({ label, icon, value, onValueChange, disabled }) => {
     const [tempUnit, setTempUnit] = useState('C');
